1881358a80c1db185143bd6d33a71a74fe2430e9
Yiqing <ywang46@uw.edu>
2022-06-05 08:51:31 -0700

    SGBM implemented

--- tk_disparity.py
+++ tk_disparity.py
@@ -24,14 +24,21 @@ def cropHorizontal(image):
             int((CAMERA_WIDTH-CROP_WIDTH)/2):
             int(CROP_WIDTH+(CAMERA_WIDTH-CROP_WIDTH)/2)]
 
-stereoMatcher = cv2.StereoBM_create()
-stereoMatcher.setMinDisparity(4)
-stereoMatcher.setNumDisparities(128)
-stereoMatcher.setBlockSize(21)
-stereoMatcher.setROI1(leftROI)
-stereoMatcher.setROI2(rightROI)
-stereoMatcher.setSpeckleRange(16)
-stereoMatcher.setSpeckleWindowSize(45)
+window_size = 3  # wsize default 3; 5; 7 for SGBM reduced size image; 15 for SGBM full size image (1300px and above); 5 Works nicely
+
+stereoMatcher = cv2.StereoSGBM_create(
+    minDisparity=-1,
+    numDisparities=6*16,
+    blockSize=window_size,
+    P1=8 * 3 * window_size,
+    P2=32 * 3 * window_size,
+    disp12MaxDiff=12,
+    uniquenessRatio=10,
+    speckleWindowSize=50,
+    speckleRange=32,
+    preFilterCap=63,
+    mode=cv2.STEREO_SGBM_MODE_SGBM_3WAY
+)
 right_matcher = cv2.ximgproc.createRightMatcher(stereoMatcher);
 
 def compute_disparity(leftFrame, rightFrame):
@@ -62,7 +69,7 @@ def compute_disparity(leftFrame, rightFrame):
 
     wls_filter = cv2.ximgproc.createDisparityWLSFilter(stereoMatcher);
     wls_filter.setLambda(8000.0);
-    wls_filter.setSigmaColor(1.5);
+    wls_filter.setSigmaColor(1.3);
     filtered_disp = wls_filter.filter(left_disp, leftFrame, disparity_map_right=right_disp);
 
     np.savez_compressed(outputFile, disparity = filtered_disp)
